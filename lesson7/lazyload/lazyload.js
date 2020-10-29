f983d8aa4653b667f4da6838120fe345a4e8fc60
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2020-10-29 10:32:41 -0600

    lazyload, index and lazyload.js

--- lesson7/lazyload/lazyload.js
+++ lesson7/lazyload/lazyload.js
@@ -1,1 +1,35 @@
-const imagesToLoad = document.querySelectorAll
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+
+
+const imgOptions = {
+    threshold: 0.5
+};
+
+const loadImages = (large) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+
+if ('IntersectionObserver' in window) {
+    const imgObserber = new IntersectionObserver ((items, imgObserver) =>){
+        items.forEach((item) => {
+            if (item.isIntersecting){
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    },imgOptions);
+
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
