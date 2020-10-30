0d397bb0833095b16e27aef6913dd18df7ccca0b
grande1988 <legrandredd@byui.edu>
2020-10-30 13:17:10 -0600

    changed whole site around

--- lesson7/lazyload/lazyload.js
+++ lesson7/lazyload/lazyload.js
@@ -1,32 +1,31 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 
 
 const imgOptions = {
-    threshold: 0.5
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
 };
 
-const loadImages = (large) => {
+const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
-        image.removeAttribute("data-src");
+        image.removeAttribute('data-src');
     };
 };
 
 
 if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver ((items, imgObserver) =>){
+    const observer = new IntersectionObserver ((items, imgObserver) =>){
         items.forEach((item) => {
             if (item.isIntersecting){
                 loadImages(item.target);
-                imgObserver.unobserve(item.target);
+                observer.unobserve(item.target);
             }
         });
-    },imgOptions);
-
-
+    });
     imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
+        observer.observe(img);
     });
  else {
     imagesToLoad.forEach((img) => {
