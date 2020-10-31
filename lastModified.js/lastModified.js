8b0f96fe90133fa90ed2668450cd03f0d10a53d9
grande1988 <legrandredd@byui.edu>
2020-10-30 23:03:01 -0600

    lastModified.js

--- lastModified.js/lastModified.js
+++ lastModified.js/lastModified.js
@@ -1,2 +1,11 @@
-let currentdate = document.lastModified;
-document.getElementById('currentdate').textContent = currentDate;
+var modDate= document.lastModified;
+var year= new Date();
+document.getElementById("lastMod").innerHTML= modDate;
+document.getElementById("copyYear").innerHTML= year.getFullYear();
+
+function wait(){
+    alert ("Too Soon! Wait another Week");
+}
+function wait2(){
+    alert ("Way to SOON! Wait two weeks!")
+}
