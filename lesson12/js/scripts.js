799688bfdc430c663d6212eae996177c54dd51f1
grande1988 <legrandredd@byui.edu>
2020-12-18 15:18:00 -0700

    toggle menu

---
+++ lesson12/js/scripts.js
@@ -0,0 +1,17 @@
+// <----------------TOGGLE MENU, HAM, AND X------------------------>
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide"); 
+  }
+  
+  function toggleHam() {
+    document.getElementById("hamID").classList.toggle("hide"); 
+  }
+  
+  function toggleX() {
+    document.getElementById("xID").classList.toggle("hide"); 
+  }
+  
+  function contactus() {
+    alert("Thank you! Your message has been sent.")
+  }
